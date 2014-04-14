564c23e7c1139d4c4edc19cb62e75f3d9ad1380b
AndrewMundy <andrew.mundy@ieee.org>
2014-04-14 13:53:38 +0100

    Use enumerated edge numberings.

--- nengo_pushbot/pushbot.py
+++ nengo_pushbot/pushbot.py
@@ -106,7 +106,7 @@ if nengo_spinnaker is not None:
         def __init__(self,
                      virtual_chip_coords=dict(x=0xFE, y=0xFF),
                      connected_node_coords=dict(x=1, y=0),
-                     connected_node_edge=0):   # 0 is EAST, 1 is NORTH_EAST
+                     connected_node_edge=common.edges.EAST):
             super(PushBotVertex, self).__init__(n_neurons=0,
                 virtual_chip_coords=virtual_chip_coords,
                 connected_node_coords=connected_node_coords,
