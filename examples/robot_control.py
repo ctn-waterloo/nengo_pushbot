15adee6f317c64a1f5d67837dfdf4fb39369b7fc
Terry Stewart <tcstewar@uwaterloo.ca>
2014-06-09 19:07:45 -0400

    Update to recent nengo

--- examples/robot_control.py
+++ examples/robot_control.py
@@ -3,17 +3,23 @@ import nengo
 import nengo_pushbot
 import numpy as np
 
-model = nengo.Network()
+model = nengo.Network(label='pushbot')
 with model:
-    input = nengo.Node(lambda t: [0.5*np.sin(t), 0.5*np.cos(t)])
-    a = nengo.Ensemble(nengo.LIF(100), dimensions=2)
+    input = nengo.Node(lambda t: [0.5*np.sin(t), 0.5*np.cos(t)], label='input')
+    a = nengo.Ensemble(100, dimensions=2, label='a')
 
-    bot = nengo_pushbot.PushBotNetwork('10.162.177.45', message_delay=0.001)
+    bot = nengo_pushbot.PushBotNetwork('ctndroid.uwaterloo.ca', port=56044, message_delay=0.001, label='Pushbot')
 
-    nengo.Connection(input, a, filter=None)
-    nengo.Connection(a, bot.tracks, filter=0.01)
+    nengo.Connection(input, a, synapse=None)
+    nengo.Connection(a, bot.tracks, synapse=0.01)
+
+import nengo_gui.javaviz
+jv = nengo_gui.javaviz.View(model)
 
 sim_normal = nengo.Simulator(model)
+jv.update_model(sim_normal)
+jv.view()
+
 sim_normal.run(5000)
 
 #import nengo_spinnaker
