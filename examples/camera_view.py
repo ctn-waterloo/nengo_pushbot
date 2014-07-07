c0a0ce269b893bb91fa4db5441b47a3f685345ff
Terry Stewart <tcstewar@uwaterloo.ca>
2014-07-07 15:34:23 -0600

    works with nengo_gui

--- examples/camera_view.py
+++ examples/camera_view.py
@@ -6,8 +6,9 @@ with model:
     bot = nengo_pushbot.PushBotNetwork('10.162.177.49')
     bot.show_image()
 
-sim = nengo.Simulator(model)
-sim.run(1000)
+if __name__ == '__main__':
+    sim = nengo.Simulator(model)
+    sim.run(1000)
 
 
 
