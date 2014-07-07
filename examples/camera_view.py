76b870bb40f7e8800490ceeb34755f54139f6153
Terry Stewart <tcstewar@uwaterloo.ca>
2014-07-07 15:33:48 -0600

    Updated to recent syntax

--- examples/camera_view.py
+++ examples/camera_view.py
@@ -3,10 +3,8 @@ import nengo_pushbot
 
 model = nengo.Network()
 with model:
-    bot = nengo_pushbot.PushBotNetwork('10.162.177.45')
-
-
-view = nengo_pushbot.RetinaView(bot.bot)
+    bot = nengo_pushbot.PushBotNetwork('10.162.177.49')
+    bot.show_image()
 
 sim = nengo.Simulator(model)
 sim.run(1000)
