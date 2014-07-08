5666e44771678f5688d3887979d34aa007ad9642
Terry Stewart <tcstewar@uwaterloo.ca>
2014-07-07 20:57:39 -0600

    Added freq tracking; support SpiNNaker config

--- nengo_pushbot/pushbot3.py
+++ nengo_pushbot/pushbot3.py
@@ -22,6 +22,15 @@ class PushBot3(object):
 
 
     def __init__(self, address, port=56000, message_delay=0.01):
+        self.image = None
+        self.regions = None
+        self.track_periods = None
+
+        if ',' in address:
+            print 'configuring for SpiNNaker', address
+            self.spinnaker_address = address
+            self.socket = None
+            return
         print 'connecting...', address
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((address, port))
@@ -44,8 +53,6 @@ class PushBot3(object):
         self.sensor = dict(compass=[0,0,0], accel=[0,0,0], gyro=[0,0,0])
         self.compass_range = None
 
-        self.image = None
-        self.regions = None
         thread.start_new_thread(self.sensor_loop, ())
 
     def count_spikes(self, **regions):
@@ -53,16 +60,29 @@ class PushBot3(object):
         self.count_regions = {}
         for k,v in regions.items():
             self.count_regions[k] = [0, 0]
+    def track_freqs(self, freqs):
+        freqs = np.array(freqs, dtype=float)
+        self.track_periods = 500000/freqs
+
+        self.last_on = np.zeros((128, 128), dtype=np.uint16)
+        self.last_off = np.zeros((128, 128), dtype=np.uint16)
+        self.p_x = np.zeros_like(self.track_periods) + 64.0
+        self.p_y = np.zeros_like(self.track_periods) + 64.0
+        self.good_events = np.zeros_like(self.track_periods, dtype=int)
+
+
 
     def get_spike_rate(self, region):
         return self.count_regions[region][0]
 
 
     def show_image(self, decay = 0.5):
+        if self.socket is None:
+            # TODO: log a warning here
+            return
         if self.image is None:
             self.image = np.zeros((128, 128), dtype=float)
             thread.start_new_thread(self.image_loop, (decay,))
-
 
 
     def image_loop(self, decay):
@@ -101,8 +121,10 @@ class PushBot3(object):
             self.compass_range = np.array([data, data], dtype=float)
         else:
             for i in range(3):
-                self.compass_range[0][i] = max(data[i], self.compass_range[0][i])
-                self.compass_range[1][i] = min(data[i], self.compass_range[1][i])
+                self.compass_range[0][i] = max(data[i],
+                                               self.compass_range[0][i])
+                self.compass_range[1][i] = min(data[i],
+                                               self.compass_range[1][i])
 
         diff = self.compass_range[0] - self.compass_range[1]
         value = [0, 0, 0]
@@ -153,7 +175,8 @@ class PushBot3(object):
                     else:
                         stop_index = len(data)
                     buffered_ascii += data[offset+index:offset+stop_index]
-                    data_all = np.hstack((data_all[:index], data_all[stop_index:]))
+                    data_all = np.hstack((data_all[:index],
+                                          data_all[stop_index:]))
                     offset += stop_index - index
                     ascii_index = np.where(data_all[::4] < 0x80)[0]
 
@@ -196,10 +219,54 @@ class PushBot3(object):
 
                 self.count_regions[k] = new_count, time
             #print {k:v[0] for k,v in self.count_regions.items()}
+        if self.track_periods is not None:
+            time = data[2::4].astype(np.uint16)
+            time = (time << 8) + data[3::4]
+            index_on = (data[1::4] & 0x80) > 0
+            index_off = (data[1::4] & 0x80) == 0
+
+            delta = np.where(index_on,
+                             time - self.last_off[x, y],
+                             time - self.last_on[x, y])
+
+            self.last_on[x[index_on],
+                         y[index_on]] = time[index_on]
+            self.last_off[x[index_off],
+                          y[index_off]] = time[index_off]
+
+            for i, period in enumerate(self.track_periods):
+                eta = 0.2
+                t_exp = period
+                sigma_t = 100
+                t_diff = delta.astype(np.float) - t_exp
+
+                w_t = np.exp(-(t_diff**2)/(2*sigma_t**2))
+                # haven't done w_p yet
+
+                # horrible heuristic for figuring out if we have good
+                # data by chekcing the proportion of events that are
+                # within sigma_t of desired period
+                self.good_events[i] += (w_t>0.5).sum()
+
+                # update position estimate
+                try:
+                    r_x = np.average(x, weights=w_t)
+                    r_y = np.average(y, weights=w_t)
+                    self.p_x[i] = (1-eta)*self.p_x[i] + (eta)*r_x
+                    self.p_y[i] = (1-eta)*self.p_y[i] + (eta)*r_y
+                except ZeroDivisionError:
+                    # occurs in np.average if weights sum to zero
+                    pass
+            print self.p_x, self.p_y
+
+
+
 
 
 
     def send(self, key, cmd, force):
+        if self.socket is None:
+            return
         now = time.time()
         if force or self.last_time.get(key, None) is None or (now >
                 self.last_time[key] + self.message_delay):
@@ -207,12 +274,14 @@ class PushBot3(object):
             self.last_time[key] = now
 
     def activate_sensor(self, name, freq):
+        if self.socket is None:       # spinnaker
+            return
         bitmask = PushBot3.sensors[name]
         period = int(1000.0/freq)
         try:
             self.socket.send('!S+%d,%d\n' % (bitmask, period))
         except:
-            self.diconnect()
+            self.disconnect()
 
     def disconnect(self):
         del PushBot3.running_bots[self.key]
@@ -258,10 +327,13 @@ class PushBot3(object):
 
 
 if __name__ == '__main__':
-    bot = PushBot3('10.162.177.49')
+    #bot = PushBot3('10.162.177.44')
+    bot = PushBot3('1,0,EAST')
     bot.activate_sensor('compass', freq=100)
     bot.activate_sensor('gyro', freq=100)
     bot.count_spikes(all=(0,0,128,128), left=(0,0,128,64), right=(0,64,128,128))
+    bot.laser(100)
+    bot.track_freqs([100])
     bot.show_image()
     import time
 
