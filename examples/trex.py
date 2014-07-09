c1da6cdcdd7f50627af4069f9c70dc8f716aed6d
Dan Neil <daniel.l.neil@gmail.com>
2014-07-08 23:00:07 -0600

    Slowing down turning, adjusting parameters.

--- examples/trex.py
+++ examples/trex.py
@@ -10,7 +10,7 @@ spinnaker = False
 model = nengo.Network(label='pushbot')
 with model:
     def inv(x):
-        return -x[0]
+        return -x
 
     # def sum_inp(x):
     #     return x[0]+x[1]
@@ -19,8 +19,8 @@ with model:
         bot = nengo_pushbot.PushBotNetwork('1,0,EAST')
     else:
         #pass
-        bot = nengo_pushbot.PushBotNetwork('10.162.177.44')
-        #bot.bot.show_image(decay=0.5)
+        bot = nengo_pushbot.PushBotNetwork('10.162.177.55')
+        bot.bot.show_image(decay=0.5)
     
     kb_input = nengo.Node([0, 0, 0, 0], label='keyboard')
     sum_left = nengo.Ensemble(100, dimensions=1, label='sum_left')
@@ -30,20 +30,20 @@ with model:
     right_drive = nengo.Ensemble(100, dimensions=1, label='right_drive')
 
     # Get turn component
-    nengo.Connection(kb_input, invert_pop, function=inv)
+    nengo.Connection(kb_input[2], invert_pop, function=inv)
 
     # Get forward component
-    nengo.Connection(kb_input[2], sum_left)
-    nengo.Connection(invert_pop,  sum_left)
-    nengo.Connection(kb_input[2],  sum_right)
+    nengo.Connection(kb_input[3], sum_left)
+    nengo.Connection(kb_input[2],  sum_left, transform=0.5)
     nengo.Connection(kb_input[3],  sum_right)
+    nengo.Connection(invert_pop,  sum_right, transform=0.5)
 
     # Bind sums to output
     nengo.Connection(sum_left, left_drive)
     nengo.Connection(sum_right, right_drive)    
 
-    nengo.Connection(left_drive, bot.motor[0], synapse=0.01, transform=0.1)
-    nengo.Connection(right_drive, bot.motor[1], synapse=0.01, transform=0.1)
+    nengo.Connection(left_drive, bot.motor[0], synapse=0.01, transform=0.5)
+    nengo.Connection(right_drive, bot.motor[1], synapse=0.01, transform=0.5)
     #nengo.Probe(a)
 
 
