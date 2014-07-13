efa69d1db6b09bab95616077046320c6b8909460
Terry Stewart <tcstewar@uwaterloo.ca>
2014-07-13 12:21:02 -0600

    Added certainty_scale parameter to PushBotNetwork

--- nengo_pushbot/network.py
+++ nengo_pushbot/network.py
@@ -27,8 +27,8 @@ class PushBotNetwork(nengo.Network):
                 node = nengo_pushbot.CountSpikes(self.bot, k, r)
                 setattr(self, 'count_%s' % k, node)
 
-    def track_freqs(self, freqs, sigma_t=100, sigma_p=30, eta=0.3):
-        self.bot.track_freqs(freqs, sigma_t=sigma_t, sigma_p=sigma_p, eta=eta)
+    def track_freqs(self, freqs, sigma_t=100, sigma_p=30, eta=0.3, certainty_scale=10000):
+        self.bot.track_freqs(freqs, sigma_t=sigma_t, sigma_p=sigma_p, eta=eta, certainty_scale=certianty_scale)
         with self:
             for i in range(len(freqs)):
                 node = nengo_pushbot.Tracker(self.bot, i)
